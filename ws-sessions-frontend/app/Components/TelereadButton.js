c3ac9fb324671245ae20be3cdf7e0378099af413
Alex Paramonov <25197861+alparamonov@users.noreply.github.com>
2024-03-24 23:59:55 +0100

    linting fix

--- ws-sessions-frontend/app/Components/TelereadButton.js
+++ ws-sessions-frontend/app/Components/TelereadButton.js
@@ -1,7 +1,5 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import CardGroup from "react-bootstrap/CardGroup";
 
 function TelereadButton(props) {
   const { isConnected, isReading, onReadClick } = props;
--- ws-sessions-frontend/app/Components/WebsocketConnector.js
+++ ws-sessions-frontend/app/Components/WebsocketConnector.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
 
@@ -13,17 +13,31 @@ function WebsocketConnector(props) {
         <Form onSubmit={connectToWs} className="">
           <Form.Group className="" controlId="formWebsocketUrl">
             <Form.Label className="text-2xl">Enter Websocket URL</Form.Label>
-            <Form.Control className="mb-2" type="text" placeholder="Websocket URL" />
+            <Form.Control
+              className="mb-2"
+              type="text"
+              placeholder="Websocket URL"
+            />
             <Form.Text className="text-xs">
               <p className="m-0">Enter your Websocket URL:</p>
-              <p className="ml-2 italic">wss://API_ID.execute-api.AWS_Region.amazonaws.com/STAGE_NAME'.</p>
-              <p>Find this information in SAM deployment output under key WebSocketURL.</p>
+              <p className="ml-2 italic">
+                wss://API_ID.execute-api.AWS_Region.amazonaws.com/STAGE_NAME'.
+              </p>
+              <p>
+                Find this information in SAM deployment output under key
+                WebSocketURL.
+              </p>
             </Form.Text>
           </Form.Group>
           <Button disabled={isConnected} variant="primary" type="submit">
             Connect
           </Button>
-          <Button className="ml-4" onClick={onDisconnectClick} disabled={!isConnected} variant="secondary">
+          <Button
+            className="ml-4"
+            onClick={onDisconnectClick}
+            disabled={!isConnected}
+            variant="secondary"
+          >
             Disconnect
           </Button>
         </Form>
